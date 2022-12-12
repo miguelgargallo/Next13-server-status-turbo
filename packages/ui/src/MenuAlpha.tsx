0ffc621b2b2db1a500280b7b918261b91cbec222
miguelgargallo <mgargallo@gmail.com>
2022-12-12 18:10:52 +0100

    2.0.0 a Pencil Ecosystem

--- packages/ui/src/MenuAlpha.tsx
+++ packages/ui/src/MenuAlpha.tsx
@@ -2,11 +2,11 @@ export const MenuAlpha = () => {
 
   return (
     <div className="flex flex-row items-center justify-between bg-gradient-to-r from-blue-400 to-blue-500">
-      <button className="px-8 py-2 font-bold text-white">
+      <button className="px-8 py-2 underline font-bold text-white">
         <a href="https://superdatas.org"
         >SuperData</a>
       </button>
-      <button className="py-2 underline font-bold text-white">
+      <button className="py-2 font-bold text-white">
         <a href="https://pylar.org"
         >PylarAI</a>
       </button>
